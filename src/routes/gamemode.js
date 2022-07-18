4c5df4cc7b67cee6fe000b65d48d4d8e7562ee02
hyeRexx <caco3.rinrin@gmail.com>
2022-07-19 03:19:13 +0900

    class Game fin ver.1

--- src/routes/gamemode.js
+++ src/routes/gamemode.js
@@ -1,4 +1,5 @@
-const socket = io();
+
+// const socket = io();
 
 class Game {
     // 게임 생성
@@ -12,15 +13,15 @@ class Game {
             maxPlayer : {value : 8, writable : false},   // 게임에 참가 가능한 최대 인원
         });
 
-        this.socket = socket;
-        this.socketAll = [];
+        this.socket = socket;   // 소켓서버
+        this.socketAll = [];    // 게임 플레이어들의 소켓 정보 배열
+        this.joinable = true;   // 게임 접근 가능 여부
+        this.playerCnt = 0;     // 게임 플레이어 수 (max 파악용)
 
         this.host = null;       // 게임을 생성한 사람 또는 첫 번째 유저
         this.mafia = null;      // 게임의 마피아 (매 게임 갱신)
         this.word = null;       // 게임 제시어 : db완료후 로직 구현 : 난수로 wordid 추출
         this.rip = [];          // 사망으로 등록된 사람
-        this.joinable = true;   // 게임에 접근 가능 여부
-        this.playerCnt = 0;     // 게임에 접속한 유저 수 (max 파악용)
 
         this.player = [];       // 게임에 접속한 유저 객체 원본
         this.turnQue = [];      // 게임 진행 순서 (queue)
@@ -36,9 +37,13 @@ class Game {
         this.socket.on("notifySelf", (user) => this.joinGame(user)); // socket.join(room)
         this.socket.on("startupRequest", () => this.startGame()); // 게임 시작 요청
         this.socket.on("openTurn", () => this.changePlayer()); // 턴 교체 요청
-        this.socket.on("citizenNight", (user) => this.voteForCitizen(user)); // 마피아 투표 띄움
-        this.socket.on("mafiaNight", (word) => this.guessForMafia(word));
+        this.socket.on("nightEvent", (user) => this.nightWork(user)); // 나잇 이벤트
         this.socket.on("newCycleRequest", () => this.openNewCycle()); // 새로운 사이클 요청
+    }
+
+    // for test
+    onAll(msg, data) {
+        return 0
     }
 
     // 방 전체에 이벤트 전송
@@ -46,32 +51,35 @@ class Game {
         this.socketAll.forEach(sock => {
             this.socket.to(sock).emit(msg, data);
         })
+
+        // for test
+        // console.log("emit event\nmsg :", msg, "\ndata :", data)
     }
 
-    // 게임 host 세팅
+    // 게임 host 세팅 : player Arr의 첫 번째 유저 (입장 순서 정렬)
     setHost() {
-        if (this.player.length === 1) {
-            this.host = this.player[0].userId;
-        }
+        this.host = this.player[0].userId;
     }
 
-    // 게임 입장 : 외부에서 매개변수로 전달한 user object를 게임에 추가
-    // on.joinGame
+    // 게임 입장 : user object에 추가 속성 부여 및 각 array에 push
     joinGame(user) { 
         user.state = false;     // 게임 in, user state 변경
 
         user.ready = false;     // 인게임용 추가 속성 : 레디 정보
         user.mafia = false;     // 인게임용 추가 속성 : 마피아 정보
         user.servived = true;   // 인게임용 추가 속성 : 살았니 죽었니
-        user.votes = 0;         // 인게임용 추가 속성 : 득표 수
+        user.votes = 0;         // 인게임용 추가 속성 : 득표 수 : 밤이되었습니다
 
         this.player.push(user);
-        this.player.push(user.socket);
+        this.turnQue.push(user.userId);
+        this.socketAll.push(user.socket);
 
         this.playerCnt++;
-        this.setHost();
+        this.joinable = (this.playerCnt < this.maxPlayer) ? true : false; // 게임 접근 차단
+        this.playerCnt == 1 ? this.setHost() : null; // 호스트 뽑기
 
-        this.socket.to(this.gameId).emit("notifyNew", user.userId);
+        // 새로운 유저 입장 알림
+        this.emitAll("notifyNew", user.userId);
     }
 
 
@@ -91,13 +99,15 @@ class Game {
         }
 
         // 다른 유저들에게 ready 알림
-        this.socket.to(this.gamdId).emit("notifyReady", user.userId, user.ready);
+        // this.socket.to(this.gamdId).emit("notifyReady", user.userId, user.ready);
+        this.emitAll("notifyReady", {user : user.userId, ready: user.ready})
 
         // 인원 조건 충족 + 마지막 ready 처리 되었을 때 readyToStart로 전달
         if (this.playerCnt > 3 && this.turnQue.length === this.playerCnt) {
             // 받아서 start button 활성화 가능
             let hostUser = this.player[this.player.findIndex(x => x.userId === this.host)]
-            this.socket.to(hostUser.socket).emit("reayToStart", true); 
+            // this.socket.to(hostUser.socket).emit("reayToStart", true);
+            this.emitAll("reayToStart", true); 
         }
     }
 
@@ -127,14 +137,15 @@ class Game {
 
     // 인게임 턴 교체 : 끝난 플레이어, 다음 플레이어 리턴 (socket.on("singleTurnChange"))
     openTurn() {
+        // 사이클 끝났는지 확인하고 notification + 사이클 끝나면 턴 제공하지 않음
+        if (this.turnCnt === this.playerCnt) {
+            this.emitAll("cycleClosed", null);
+            return;
+        }
+
         let nowPlayer = this.turnQue.shift(); // 리턴해 줄 유저! 지금 그릴 사람!
         this.turnQue.push(nowPlayer); // 뽑고 바로 뒤로 밀어넣기
         let isMafia = (nowPlayer === this.mafia) ? true : false; // 마피아인지 확인
-        
-        // 사이클 끝났는지 확인하고 notification
-        if (this.turnCnt === this.playerCnt) {
-            this.emitAll("cycleClosed", null)
-        }
         
         this.turnCnt++;
 
@@ -142,7 +153,7 @@ class Game {
         this.emitAll("singleTurnInfo", data)
     }
 
-    // 인게임 밤이되었습니다 : 시민 - 투표 / 마피아 - 제시어 맞추기
+    // 인게임 : 밤이되었습니다 : 시민 - 투표 / 마피아 - 제시어 맞추기
     nightWork(userId, submit) {
         let userIdx = this.player.findIndex(x => x.userId === user);
         let user = player[userIdx];
@@ -178,7 +189,7 @@ class Game {
                 });
             }
 
-            // 초기화 
+            // night result 초기화 
             this.guessRst = false;
             this.voteRst = null;
 
@@ -191,18 +202,16 @@ class Game {
     // mafia or not으로 조건 분기해서 front로 emit userid
     voteForCitizen(user) {
         let userIdx = this.player.findIndex(x => x.userId === user);
-        let user = player[userIdx];
-        user.votes++; // 득표 수++
-        
-        // 투표 수 충족시
-        // 사망 또는 체포 분기 : 아무 이벤트도 없는 경우는 client에서 캐치해 주어야 함
-        if (user.votes == this.playerCnt - 1) {
-            this.voteRst = user.userId;
+        let gameuser = player[userIdx];
+        gameuser.votes++; // 득표 수++
+    
+        // 투표 수 충족시 : 사망 또는 체포 분기
+        if (gameuser.votes == this.playerCnt - 1) {
+            this.voteRst = gameuser.userId;
         }
     }
 
-    // 제시어 추측 : 마피아만
-    // success or not으로 조건분기해서 front로 emit
+    // 제시어 추측 : 마피아만 : success or not으로 조건분기해서 front로 emit
     guessForMafia(word) {
         if (word === this.word) {
             this.guessRst = true;
@@ -211,9 +220,9 @@ class Game {
 
     // 새로운 턴 시작
     openNewCycle() {
-        this.turnCnt = 0;
-        this.cycleCnt++;
-        this.openTurn();
+        this.turnCnt = 0;   // 초기화
+        this.cycleCnt++;    // cycle 정보 업데이트 : 추후 마피아 힌트 등 준비
+        this.openTurn();    // 새로운 턴 정보 제공
     }
 
     // 게임 종료 : 정상 종료
@@ -243,10 +252,13 @@ class Game {
         // 인게임용 속성 제거 : ready
         delete this.player[userIdx].ready;
         delete this.player[userIdx].mafia;
+        delete this.player[userIdx].servived;
+        delete this.player[userIdx].votes;
 
         this.playerCnt--;
 
-        this.socket.to(this.gameId).emit("otherExit", userId);
+        // this.socket.to(this.gameId).emit("otherExit", userId);
+        this.emitAll("otherExit", userId);
     }
 
 
