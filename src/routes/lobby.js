86bd6721d4dd84261ac05e62235b6fb1c7d54a54
Ubuntu <ubuntu@ip-172-31-40-215.ap-northeast-2.compute.internal>
2022-07-24 10:49:30 +0000

    from enter to game start

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -4,16 +4,16 @@ import {userInfo} from '../server.js';
 const router = express.Router();
 const dbpool = require('../lib/db');
 
-router.post("/profile_img", async(req, res) => {
-    try {
-        const [[data]] = await dbpool.query('SELECT profile_img FROM `USER` WHERE  `userid` = ?', req.body.userId);
-        return res.json(data);
-      } catch(err){
-        res.send('에러!');
-      }
-});
+// 로그인 성공시 바로 세팅되게 해놓아서 불필요해짐
+// router.post("/profile_img", isLoggedIn, async(req, res) => {
+//     try {
+//         return res.json(req.user.profile_img);
+//       } catch(err){
+//         res.send('에러!');
+//       }
+// });
 
-router.post('/friendinfo', async (req, res) => {
+router.post('/friendinfo', isLoggedIn, async (req, res) => {
   try {
     const [data] = await dbpool.query('SELECT userid FROM USER a join CITIZEN b on a.id = b.friendid where b.myid=4;');
     // console.log("asdfasdfff", data);
